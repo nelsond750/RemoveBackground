425f5268e37e9d14b2946b0a56d0defef77a6510
Nelson Salvador <nelsond750@gmail.com>
2026-02-11 15:54:28 +0100

    Add CORS configurações

--- background-remover/backend/api/main.py
+++ background-remover/backend/api/main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI,UploadFile,File
 from fastapi.responses import StreamingResponse
 from rembg import remove
@@ -5,6 +6,15 @@ from PIL import Image
 import io
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ],
+    allow_methods=["POST"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 def health():
