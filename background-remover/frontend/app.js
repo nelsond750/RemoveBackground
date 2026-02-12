38fe368096f3824280eae4f37c8aa310da6f7fd6
Nelson Salvador <nelsond750@gmail.com>
2026-02-12 14:09:45 +0100

    Nova versão

--- background-remover/frontend/app.js
+++ background-remover/frontend/app.js
@@ -1,26 +1,26 @@
-const imageInput = document.getElementById("imgeIpunt");
+const imageInput = document.getElementById("imageIpunt");
 const removeBtn = document.getElementById("removebtn");
-const resuktImage = document.getElementById("resultImage");
+const resultImage = document.getElementById("resultImage");
 const downloadLink = document.getElementById("downloadLink");
 
 removeBtn.addEventListener("click", async() =>{
-    const file = imageInput.file[0]
+    const file = imageInput.files[0]
 
     if(!file){
         return;
     }
-    const formaDtat = new FormData();
+    const formaData = new FormData();
     formaData.append("file", file);
 
-    const response = await fetch("https://localhost:8000/remover-bg" {
-    method: "POST",
-    body:FormData,    
+    const response = await fetch("https://localhost:8000/remover-bg", {
+        method: "POST",
+        body:FormData,    
     });
 
 
 const blob = await response.blob();
 const imageUrl = URL.createObjectURL(blob);
-resuktImage.src = imageUrl;
+resultImage.src = imageUrl;
 downloadLink.href = imageUrl;
 downloadLink.style.display = "inline";
 });
